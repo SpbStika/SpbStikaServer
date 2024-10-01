d7d438a145e7783842193f2c3574c4604cb3e060
SpbStika <stika2007@mail.ru>
2024-10-01 20:13:27 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 mustpl -d '{"SERVER": "${SERVER:-SET_SERVER}", "PUBLIC_KEY": "${PUBLIC_KEY:-SET_PUBLIC_KEY}", "NAME": "${NAME:-ss}"}' -o /bin/show /opt/show-template
 chmod +x /bin/show
@@ -8,10 +8,8 @@ chmod +x /bin/show
 # elif [ ! -f /etc/sing-box/config.json ] && [ "${PROTOCOL}" = "vless" ]; then
 #     mustpl -d '{"UUID": "${UUID:-SET_UUID}", "PRIVATE_KEY": "${PRIVATE_KEY:-SET_PRIVATE_KEY}", "SHORT_ID": "${SHORT_ID:-153bb5b1383b79fd}", "FAKE_SERVER": "${FAKE_SERVER:-www.google.com}", "PORT": "${PORT:-443}", "NAME": "${NAME:-vless}"}}' -o /etc/sing-box/config.json /opt/config-template-vless.json
 # fi
-S_Users = ""
-S_Users += "${USERS}"
 
-IFS="," read -r -a Users_array <<< "${S_Users}"
+IFS="," read -r -a Users_array <<< "${USERS}"
 for User in "${Users_array[@]}";
 do
  echo "${User}"
