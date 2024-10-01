911e2811ad05b2911d40a4cb8672f926b5cbf6a5
SpbStika <stika2007@mail.ru>
2024-10-02 00:30:28 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ chmod +x /bin/show
 #     mustpl -d '{"UUID": "${UUID:-SET_UUID}", "PRIVATE_KEY": "${PRIVATE_KEY:-SET_PRIVATE_KEY}", "SHORT_ID": "${SHORT_ID:-153bb5b1383b79fd}", "FAKE_SERVER": "${FAKE_SERVER:-www.google.com}", "PORT": "${PORT:-443}", "NAME": "${NAME:-vless}"}}' -o /etc/sing-box/config.json /opt/config-template-vless.json
 # fi
 
-IFS="," read -ra Users_array <<< "${USERS}"
+readarray -d ',' -t Users_array < <(printf '%s' "${USERS}")
 for User in "${Users_array[@]}";
 do
  echo "${User}"
 done
 
-exec "$@"
+
